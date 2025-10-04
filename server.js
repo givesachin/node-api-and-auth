b05e04633279fde668f8192a407c9b472b29f6e3
Sachin Bhoi <135997791+givesachin@users.noreply.github.com>
2025-10-04 16:53:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -93,4 +93,7 @@ app.post('/items/delete', authenticateToken, async (req, res) => {
 // -------------------
 // Start Server
 // -------------------
-app.listen(PORT, '0.0.0.0
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
